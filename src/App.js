94bc4fc1f9215b9168132195013fb57a472ba726
Michael Walter <michael_patrick.walter@outlook.com>
2020-10-15 16:12:00 -0700

    Created project files, changed page title, and removed default App.js script.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>MPAW Porfolio Ver_3.1</h1>
+    </div>
+  );
+}
+
+export default App;
