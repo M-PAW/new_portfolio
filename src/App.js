4bca1cf0edd9c7df02d35dcd9db31c53362b22b0
Michael Walter <michael_patrick.walter@outlook.com>
2020-10-26 23:17:40 -0700

    Built out desktop view for components: Home, Education, Home. Still working on building new tests, genertic coverage already up and running.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import './App.css';
-import Navigation from './main-components/Navigation';
-import Home from './main-components/Home';
-import Education from './main-components/Education';
+import './main-styles/common-styles.css';
+//import Navigation from './main-components/Navigation';
+// import Home from './main-components/Home';
+// import Education from './main-components/Education';
 import Skills from './main-components/Skills';
-import Projects from './main-components/Projects';
-import Social from './main-components/Social';
+// import Projects from './main-components/Projects';
+// import Social from './main-components/Social';
 
 function App() {
   return (
-    <div className="App" data-test="component-app">
-      <Navigation />
-      <Home />
-      <Education />
+    <div className="main-container" data-test="component-app">
+      {/* <Navigation /> */}
+      {/* <Home /> */}
+      {/* <Education /> */}
       <Skills />
-      <Projects />
-      <Social />
+      {/* <Projects /> */}
+      {/* <Social /> */}
     </div>
   );
 }
--- src/main-components/Home.js
+++ src/main-components/Home.js
@@ -1,10 +1,19 @@
 
 import React from 'react';
 
+import '../main-styles/common-styles.css'
+import '../main-styles/home.css';
+
 const Home = () => {
     return (
-        <div data-test="component-home">
-            <h2>Homepage Component</h2>
+        <div className="content-container home" data-test="component-home">
+            <h3>Greetings and welcome to my home(page).</h3>
+            <h3 className="mandarin">您好，欢迎到我的网站</h3>
+            <h4>My name is Michael Walter, and I'm a Full-Stack Developer.</h4>
+            <h4 className="mandarin">我的名字是王惟之， 我是全栈网站开发人员。</h4>
+            <h5>Please, take a look around and get to know more me.</h5>
+            <h5>请你看一下和认识我</h5>
+            
         </div>
     )
 }
